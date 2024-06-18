13505218b0632fbd3a72f6d712ab5908ff1c76b6
baisampayans <baisampayans@gmail.com>
2024-06-18 22:27:04 +0530

    Initial Commit

---
+++ WebServiceApp/WebServiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebServiceAppApp.swift
+//  WebServiceApp
+//
+//  Created by Baisampayan Saha Ultra on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WebServiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
